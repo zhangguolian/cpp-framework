ad2295ac055940b4b5e289aba05d04f8bab53f08
Derek <286326875@qq.com>
2018-09-14 12:34:31 +0800

    不定参数支持不传

--- logs/logs.hpp
+++ logs/logs.hpp
@@ -12,17 +12,17 @@ bool is_debug() {
 
 #define LOG(type, format, ...)\
     printf("%s [%s] [%s:%d]:", utils::GetDate().c_str(), #type, __FUNCTION__, __LINE__);\
-    printf(format, __VA_ARGS__);
+    printf(format, ##__VA_ARGS__);
 
 #define LOG_DEBUG(format, ...)\
     if (is_debug()) {\
-        LOG(debug, format, __VA_ARGS__);\
+        LOG(debug, format, ##__VA_ARGS__);\
     }   
 
 #define LOG_INFO(format, ...)\
-    LOG(info, format, __VA_ARGS__)
+    LOG(info, format, ##__VA_ARGS__)
 
 #define LOG_ERROR(format, ...)\
-    LOG(error, format, __VA_ARGS__)
+    LOG(error, format, ##__VA_ARGS__)
 
 
--- tests/logs/main.cpp
+++ tests/logs/main.cpp
@@ -5,7 +5,6 @@ int main() {
     //printf("\033[31mhello world\033[5m\n");
     LOG_DEBUG("%d\n", 1);
     LOG_INFO("%d\n", 2);
-    LOG_ERROR("%d\n", 3);
-
+    LOG_ERROR("test\n");
     return 0;
 }
