6517170f28587d76a12418d66bd76a3833242ad4
Derek <286326875@qq.com>
2018-09-20 15:20:00 +0800

    修改rpc

--- rpc/rpc.h
+++ rpc/rpc.h
@@ -5,6 +5,30 @@
 #define START_RPC_SERVER(port)\
     rpc::RpcServer::GetInstance()->Run(port);
 
+#define RPC_SERVICE(service)\
+class Rpc##service final : public service::Service
+
+#define RPC_METHOD(service, method)\
+grpc::Status method(grpc::ServerContext* context,\
+                    const service##Request* request,\
+                    service##Response* response) override;
+
+#define RPC_SERVICE_DEFINE(service)\
+class RpcTmp##service {\
+public:\
+    RpcTmp##service() {\
+        rpc::RpcServer::GetInstance()->RegisterService(&service_);\
+    }\
+    Rpc##service service_;\
+};\
+RpcTmp##service g_rpc_##service;
+
+#define RPC_METHOD_DEFINE(service, method)\
+grpc::Status Rpc##service::method(grpc::ServerContext* context,\
+                                  const service##Request* request,\
+                                  service##Response* response)
+
+
 #define RPC_SERVICE_METHOD(service, method)\
 class Rpc##service##method final : public service::Service {\
     grpc::Status method(grpc::ServerContext* context,\
--- tests/rpc/server/main.cpp
+++ tests/rpc/server/main.cpp
@@ -2,7 +2,19 @@
 #include <rpc/rpc.h>
 #include <tests/rpc/rpc.grpc.pb.h>
 
-RPC_SERVICE_METHOD(RpcTestService, Search) {
+// RPC_SERVICE_METHOD(RpcTestService, Search) {
+//     printf("RpcTestService Search req:%s\n", request->request().c_str());
+//     response->set_result("success");
+//     return rpc::Status::OK;
+// }
+
+RPC_SERVICE(RpcTestService) {
+    RPC_METHOD(RpcTestService, Search);
+};
+
+RPC_SERVICE_DEFINE(RpcTestService);
+
+RPC_METHOD_DEFINE(RpcTestService, Search) {
     printf("RpcTestService Search req:%s\n", request->request().c_str());
     response->set_result("success");
     return rpc::Status::OK;
